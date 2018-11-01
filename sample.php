9332d775ac53a4750845e953c4c22f90fb202641
田　英毅 <denhideki@tahidekinoMacBook-Pro.local>
2018-11-01 14:42:48 +0900

    簡単なレビューアプリを作りました

--- sample.php
+++ sample.php
@@ -1,25 +1,47 @@
 <?php
 
 
-  function write_review() {
-    post = array();
+  function write_review($a_posts) {
+    $post = array();
+    echo "映画のタイトルを入力してください",PHP_EOL;
+    $post['title'] = trim(fgets(STDIN));
+    echo "映画のジャンルを入力してください",PHP_EOL;
+    $post['genre'] = trim(fgets(STDIN));
+    echo "映画の感想を入力してください",PHP_EOL;
+    $post['review'] = trim(fgets(STDIN));
 
+    echo $post['title'];
+    $a_posts[] = $post;
+
+
+
+    return $a_posts;
   }
 
 
 
-  function read_review(){
-    echo "こんにちは２"
+  function read_review($show_posts){
+   $count = 0;
+   $line = "-------------------------";
+   echo "見たいレビュー番号を選択してください";
+    foreach ($show_posts as $post) {
+      echo "{$count}：{$post['title']}のレビュー",PHP_EOL;
+      $count += 1;
+    }
+
+    $input = intval(trim(fgets(STDIN)));
+    $post = $show_posts[$input];
+    echo $line,PHP_EOL;
+    echo "タイトルは{$post['title']}です",PHP_EOL;
+    echo "ジャンルは{$post['genre']}です",PHP_EOL;
+    echo "感想は{$post['review']}です",PHP_EOL;
+    echo $line,PHP_EOL;
   }
 
 
 
 
-
-
-
-
-$posts = array();
+ $posts = array();
 
 while (true) {
   echo "これは映画ランキングアプリです",PHP_EOL;
@@ -29,19 +51,14 @@ while (true) {
   echo "[2]アプリを終了させる",PHP_EOL;
 
   $input = intval(trim(fgets(STDIN)));
-  echo "入力された番号は{$input}です",PHP_EOL;
 
-
-  if ($input === 0){
-    echo "{$input}番です",PHP_EOL;
-　　      write_review();
-  }elseif ($input === 1) {
-    echo "{$input}番です",PHP_EOL;
-    read_review();
-  }elseif ($input === 2){
-     echo "{$input}番です",PHP_EOL;
+  if ($input === 0) {
+     $posts = write_review($posts);
+  } elseif ($input === 1) {
+      read_review($posts);
+  } elseif ($input === 2) {
      exit;
-  }else{
+  } else {
      echo "{$input}番は無効な値です",PHP_EOL;
   }
 
